d59e96099bba2cc772a03331939575928736a074
Anuj Jadhav <114192046+Anuj-gif-web@users.noreply.github.com>
2024-09-30 13:08:29 -0400

    Update firebaseconfig.js
    Removed Firebase config

--- firebase/firebaseconfig.js
+++ firebase/firebaseconfig.js
@@ -7,13 +7,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyChZuO8bU5yIndByodLwETLKFXKS4kJmSA",
-  authDomain: "helpunity-eed68.firebaseapp.com",
-  projectId: "helpunity-eed68",
-  storageBucket: "helpunity-eed68.appspot.com",
-  messagingSenderId: "504075592698",
-  appId: "1:504075592698:web:c6afae52d6c23adf9c3a43",
-  measurementId: "G-ZWSBY8YPGF"
+
 };
 
 let app;
